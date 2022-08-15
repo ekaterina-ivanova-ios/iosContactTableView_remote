5e5e1d5cf2f7a2bc9fdf5c278e62468b168b2966
ekaterina-ivanova-ios <hard123hatiko@gmail.com>
2022-08-15 18:29:34 +0300

    add window into app delegate

--- iosContactTableView/AppDelegate.swift
+++ iosContactTableView/AppDelegate.swift
@@ -9,11 +9,15 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame:UIScreen.main.bounds)
+        //        window?.rootViewController = ContactsViewController()
+        window?.rootViewController = UINavigationController(rootViewController: UIViewController())
+        
+        window?.makeKeyAndVisible()        // Override point for customization after application launch.
         return true
     }
 
