b5511048b3e3f7c088064e3679be46b5595d313b
ekaterina-ivanova-ios <hard123hatiko@gmail.com>
2022-08-16 09:46:04 +0300

    Add pictures for avatars and countries, add new setting

--- iosContactTableView/AppDelegate.swift
+++ iosContactTableView/AppDelegate.swift
@@ -14,8 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame:UIScreen.main.bounds)
-        window?.rootViewController = UIViewController()
+        //window?.rootViewController = ContactsViewController()
+        //window?.rootViewController = UINavigationController(rootViewController: ContactsViewController())
+        window?.rootViewController = UINavigationController(rootViewController: ContactsViewController())
         window?.makeKeyAndVisible()
+        
         return true
     }
 
--- iosContactTableView/ContactsViewController.swift
+++ iosContactTableView/ContactsViewController.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import UIKit
+
+class ContactsViewController: UIViewController {
+    
+    //create array with contacts
+    private let contacts = ContactAPI.getContacts()
+    //create table
+    let contactsTableView = UITableView()
+    
+    //setting view
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(contactsTableView)
+        
+        //setting table
+        contactsTableView.translatesAutoresizingMaskIntoConstraints = false
+        contactsTableView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor).isActive = true
+        contactsTableView.leadingAnchor.constraint(equalTo:view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        contactsTableView.trailingAnchor.constraint(equalTo:view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        contactsTableView.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        contactsTableView.delegate = self
+        contactsTableView.dataSource = self
+        
+    }
+}
+ 
+
+extension ContactsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        cell.textLabel?.text = contacts[indexPath.row].name
+        
+        return cell
+    }
+    
+        
+}
